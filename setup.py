10f56699bfd22745cfeb711d6dee5f0c46c85dc9
Malte Mosler <20888573+maltemosler@users.noreply.github.com>
2021-03-20 10:38:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='UltimateSelenium',
-    version='1.1.0',
+    version='4.0.0',
     url='https://github.com/maltemosler/UltimateSelenium.git',
     author='Malte Mosler',
     author_email='support@skin-society.com',
